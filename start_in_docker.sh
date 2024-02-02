bafeb5951835b388b3ab93a406bd674fd32aafb7
artem78 <megabyte1024@ya.ru>
2024-02-02 20:28:52 +0300

    timezone

--- start_in_docker.sh
+++ start_in_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-docker run -v my_data_vol:/data -p 7275:7275 -i -d supl-proxy:latest -s supl.google.com:7275 -v --tls -p 7275 -l /data/supl_proxy.log
+docker run -v my_data_vol:/data -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro -p 7275:7275 -i -d supl-proxy:latest -s supl.google.com:7275 -v --tls -p 7275 -l /data/supl_proxy.log
